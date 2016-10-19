4a20a79db9f003c556b4f41474552acf729e19e4
superducktoes <nroy@tns3086l.corp.tenablesecurity.com>
2016-10-19 14:32:04 -0400

    updated command line options and imports
    - added command line options for launching a scan form the command line
    - added command line help option
    - removed url lib import and added verify option

--- nessusScan.py
+++ nessusScan.py
@@ -1,16 +1,15 @@
 import json
 import requests
-import urllib
 import sys #used for command line arguments
 
+#replace these with the keys for the account used for scanning
 accessKey = ""
 secretKey = ""
 
 url = "https://cloud.tenable.com/"
 headers = {'X-ApiKeys': 'accessKey=' + str(accessKey) + '; secretKey = ' + str(secretKey) + ';'}
 
-requests.packages.urllib3.disable_warnings()
-
+#gets a list of available policies
 def listPolicies():
 	policies = requests.get(url+"policies/",headers=headers,verify=True)
 	listPolicies = policies.json()["policies"]	
@@ -22,15 +21,17 @@ def listPolicies():
 	
 	return policyChoice
 
+#used to get the uuid of the template once we have the policy id
 def templateUuid(policyChoice):
-	templateInfo = requests.get(url+"policies/"+str(policyChoice),headers=headers)
+	templateInfo = requests.get(url+"policies/"+str(policyChoice),headers=headers,verify=True)
 	templateInfo = templateInfo.json()["uuid"]
 	templateUuid = str(templateInfo)
 	
 	return templateUuid
-		
+
+#gets a list of available cloud/local scanners.		
 def listScanners():
-	scanners = requests.get(url+"scanners/",headers=headers,verify=False)
+	scanners = requests.get(url+"scanners/",headers=headers,verify=True)
 	listScanners = scanners.json()["scanners"]
 
 	for i in listScanners:
@@ -40,7 +41,8 @@ def listScanners():
 
 	return scannerChoice
 
-
+# Scan class. takes in the policy id, scanner id of the scanner to use, and the ips to scan
+# launchScan() is used to kick off the scan
 class Scan:
 	
 	def __init__(self,policyChoice,scannerChoice,ipsToScan):
@@ -60,7 +62,8 @@ class Scan:
 	
 	def displayUuid(self):
 		return self.uuid
-
+	
+	#launches the scan in nessus
 	def launchScan(self):
 		scan = {"uuid":self.uuid,
 			"settings": {
@@ -72,23 +75,35 @@ class Scan:
 			"launch_now":"true"}
 			}
 		scanData = requests.post(url+"scans",json=scan,headers=headers,verify=True)
-		
+
+		#checks to make sure that there weren't any errors when adding/kicking off the scan
+		if(scanData.status_code == 200):
+			print("Scan launched successfully")
+		else:
+			print("Error with launching the scan")		
 
 if __name__ == '__main__':
 
-#	if(len(sys.argv) < 3 or len(sys.argv) > 4):
-	ipsToScan = input("Enter the ip/ip's to scan: ")
+	if((len(sys.argv) != 4) and (sys.argv[1] != "help")):
+		ipsToScan = input("Enter the ip/ip's to scan: ")
 		#list all of the available scan policies
-	policyChoice = listPolicies()
-	scannerChoice = listScanners()	
-	scanUuid = templateUuid(policyChoice)
+		policyChoice = listPolicies()
+		scannerChoice = listScanners()	
+		scanUuid = templateUuid(policyChoice)
 		
-	print("UUID :",scanUuid)	
+		print("UUID :",scanUuid)	
 		#Create the scan
-	newScan = Scan(policyChoice,scannerChoice,ipsToScan)	
-	print("Using scanner: ", newScan.displayScanner())
-	print("Using UUID: ",newScan.displayUuid())
-	newScan.launchScan()			
-#	else:
-#		newScan = Scan(str(sys.argv[0]),str(sys.argv[1]),str(sys.argv[2]))
-#		print("Using scanner: ", newScan.displayScanner())
+		newScan = Scan(policyChoice,scannerChoice,ipsToScan)	
+		newScan.launchScan()		
+	#displays infomration about command line arguments	
+	elif(sys.argv[1] == "help"):
+		print("arg1 - Scan policy ID")
+		print("arg2 - Scanner choice ID")
+		print("arg3 - IP or cidr to scan")
+	#if there are command line args then we can go ahead and launch the scan
+	else:
+		policyChoice = sys.argv[1]
+		scannerChoice = sys.argv[2]
+		ipsToScan = sys.argv[3]
+		newScan = Scan(policyChoice,scannerChoice,ipsToScan)
+		newScan.launchScan()		
